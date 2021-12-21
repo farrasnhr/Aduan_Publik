6cc9c623de694d90102cec1ec046e02ab92dfa89
Hasymi Al <68429761+zxyvyx@users.noreply.github.com>
2021-12-21 23:49:25 +0700

    Re-add: form aduan

--- app/controllers/Aduan.php
+++ app/controllers/Aduan.php
@@ -1,8 +1,8 @@
 <?php
 class Aduan extends Controller {
-    public function index()
-    {
+    public function index() {
         $data['judul'] = "Aduan";
+        $data['css'] = "aduan";
         $this->view('templates/header', $data);
         $this->view('aduan/index');
         $this->view('templates/footer');
--- app/core/Constant.php
+++ app/core/Constant.php
@@ -1,2 +1,5 @@
 <?php
     define('BASEURL', 'http://localhost/Aduan_Publik/public/');
+    define('baseCSS', '<link rel="stylesheet" href="');
+    define('endCSS', '">');
+    define('CSS', 'http://localhost/Aduan_Publik/public/css/');
