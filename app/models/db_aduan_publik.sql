2ffc57285aebaf62ece3a154bd6294cc3fecf319
Hasymi Al <68429761+zxyvyx@users.noreply.github.com>
2022-01-28 05:02:48 +0700

    Update: template database

--- app/models/db_aduan_publik.sql
+++ app/models/db_aduan_publik.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.4
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 02, 2022 at 08:19 AM
--- Server version: 10.4.17-MariaDB
--- PHP Version: 8.0.1
+-- Generation Time: Jan 27, 2022 at 11:02 PM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -70,10 +70,20 @@ CREATE TABLE `aduan` (
 --
 
 INSERT INTO `aduan` (`id_aduan`, `judul`, `isi`, `tanggal_aduan`, `lokasi`, `status_anonim`, `status_rahasia`, `status_aduan`, `id_user`, `id_kategori`, `id_instansi`, `id_klasifikasi`) VALUES
-(1, 'Aduan 1', 'Isi aduan 1', '2022-01-01', 'Lokasi 1', 0, 0, 'Menunggu', 1, 1, 1, 1),
+(1, 'Aduan 1', 'Isi aduan 1', '2022-01-01', 'Lokasi 1', 0, 0, 'Direspon', 1, 1, 1, 1),
 (2, 'Aduan 2', 'Isi aduan 2', '2021-12-22', 'Lokasi 2', 1, 1, 'Direspon', 2, 2, 2, 2),
-(3, 'Aduan 3', 'Isi aduan 3', '2021-12-29', 'Lokasi 3', 1, 0, 'Direspon', 1, 2, 3, 2),
-(4, 'Aduan 4', 'Isi aduan 4', '2021-12-03', 'Lokasi 3', 0, 0, 'Menunggu', 2, 2, 4, 3);
+(3, 'Aduan 3', 'Isi aduan 3', '2021-12-29', 'Lokasi 3', 1, 0, 'Ditolak', 1, 2, 3, 2),
+(4, 'Aduan 4', 'Isi aduan 4', '2021-12-03', 'Lokasi 3', 0, 0, 'Menunggu', 2, 2, 4, 3),
+(6, 'coba', 'tes', '2022-01-28', 'tes', 0, 0, 'Ditolak', 9, 1, 3, 2),
+(8, 'tse', 'tes', '2022-01-28', 'tes', 0, 0, 'Direspon', 2, 1, 3, 2),
+(9, 'lagi', 'lagi', '2022-01-28', 'sss', 0, 0, 'Direspon', 2, 1, 3, 2),
+(10, 'tes', 'tes', '2022-01-28', 'ssssss', 0, 0, 'Direspon', 2, 1, 3, 2),
+(11, 'tes', 'tes', '2022-01-28', 'aaaa', 0, 0, 'Menunggu', 2, 1, 3, 2),
+(12, 'aaa', 'aaaa', '2022-01-28', 'ssss', 0, 0, 'Menunggu', 2, 1, 3, 2),
+(13, 'tes', 'tes', '2022-01-28', 'tes', 0, 0, 'Menunggu', 2, 1, 3, 2),
+(14, 'aa', 'sfdasd', '2022-01-28', 'dasd', 1, 1, 'Menunggu', 2, 1, 3, 2),
+(15, 'ss', 'tes', '2022-01-28', 'qwe', 1, 1, 'Menunggu', 11, 2, 4, 1),
+(16, 'aaa', 'qwe', '2022-01-28', 'qwe', 0, 0, 'Menunggu', 11, 2, 2, 3);
 
 -- --------------------------------------------------------
 
@@ -168,12 +178,12 @@ INSERT INTO `kurasi` (`id_kurasi`, `id_aduan`, `id_admin`, `tanggal_respon`) VAL
 
 CREATE TABLE `users` (
   `id_user` int(11) NOT NULL,
-  `nik` varchar(17) NOT NULL,
   `nama` varchar(100) NOT NULL,
-  `gender` enum('pria','wanita') NOT NULL,
-  `pekerjaan` varchar(100) NOT NULL,
+  `gender` enum('pria','wanita','lainnya') NOT NULL,
   `tanggal_lahir` date NOT NULL,
+  `alamat` varchar(500) NOT NULL,
   `no_telp` varchar(12) NOT NULL,
+  `pekerjaan` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
   `password` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -182,9 +192,37 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id_user`, `nik`, `nama`, `gender`, `pekerjaan`, `tanggal_lahir`, `no_telp`, `email`, `password`) VALUES
-(1, '1234567896541235', 'User 1', 'pria', 'Pekerjaan 1', '2000-05-17', '08126541234', 'user1@mail.com', 'passworduser1'),
-(2, '1234568796541236', 'User 2', 'wanita', 'Pekerjaan 2', '2003-02-05', '085965423598', 'user2@mail.com', 'passworduser2');
+INSERT INTO `users` (`id_user`, `nama`, `gender`, `tanggal_lahir`, `alamat`, `no_telp`, `pekerjaan`, `email`, `password`) VALUES
+(1, 'User 1', 'pria', '2000-05-17', '', '08126541234', 'Pekerjaan 1', 'user1@mail.com', 'passworduser1'),
+(2, 'User 2', 'wanita', '2003-02-05', '', '085965423598', 'Pekerjaan 2', 'user2@mail.com', 'passworduser2'),
+(3, 'Meong', 'pria', '2002-05-11', 'Jonggol', '0877777', 'Mengeong', 'meong@meong.nya', 'meong'),
+(4, '123', 'pria', '2022-01-19', 'aaa', '123123', '123', 's@d.ccc', '123'),
+(5, 'abc', 'pria', '2022-01-27', 'abc', '123', 'abc', 'mail1@mail.com', '123'),
+(7, 'tes', 'pria', '2022-01-27', 'def', '123', 'tes', 'tes@tes.com', 'tes'),
+(8, 'abc', 'pria', '2022-01-27', 'abc', '+62812365945', 'abc', 'abc@abc.com', 'abc'),
+(9, 'coba', 'pria', '2022-01-27', 'coba', '086956231254', 'coba', 'coba@mail.com', '$2y$10$GcpDoJ4zrYTBc5amIjyh.OCGoyyr33CBQ0bGIahieUWO7YnY8sn8u'),
+(10, 'tes1', 'pria', '2022-01-27', 'tes1', '086545698745', 'tes1', 'tes1@tes1.com', '$2y$10$M/IniVdMH1QJrgLFC9lMse.DDzA3azD/W0.VKp/7iVCJRWar7ZZbm'),
+(11, 'Leonardo', 'pria', '2022-01-27', 'Italia', '+62851568756', 'Pelukis', 'leonardo@gmail.com', '$2y$10$0OC.JvI2jm1rvAWmAYTnRuZgvqvXzuIWVqIAsDJozZ9mpYXOky/qm'),
+(14, 'tes', 'pria', '2022-01-28', 'tes', '+62865978546', '', 'kosong@mail.com', '$2y$10$xLZtm7knr9RrH4VWiy8Zg.nuaDEbB.uHDt4IA9MWv3haszRXp9Lfy');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_role`
+--
+
+CREATE TABLE `user_role` (
+  `id_role` int(11) NOT NULL,
+  `role` varchar(128) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user_role`
+--
+
+INSERT INTO `user_role` (`id_role`, `role`) VALUES
+(1, 'Admin'),
+(2, 'Member');
 
 --
 -- Indexes for dumped tables
@@ -239,6 +277,12 @@ ALTER TABLE `users`
   ADD PRIMARY KEY (`id_user`);
 
 --
+-- Indexes for table `user_role`
+--
+ALTER TABLE `user_role`
+  ADD PRIMARY KEY (`id_role`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -252,7 +296,7 @@ ALTER TABLE `admins`
 -- AUTO_INCREMENT for table `aduan`
 --
 ALTER TABLE `aduan`
-  MODIFY `id_aduan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_aduan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `instansi`
@@ -282,7 +326,13 @@ ALTER TABLE `kurasi`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT for table `user_role`
+--
+ALTER TABLE `user_role`
+  MODIFY `id_role` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Constraints for dumped tables
